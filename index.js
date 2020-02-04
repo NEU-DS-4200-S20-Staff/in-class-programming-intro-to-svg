6ca3e3bd3f7a02ee6a6c89c92155586c4ddf443b
Dylan Cashman <dylan.cashman@tufts.edu>
2020-02-04 10:25:25 -0500

    Adding rectangle inside the SVG and adding random color to newly generated points

--- index.js
+++ index.js
@@ -1,20 +1,20 @@
 const colorScale = d3.scaleOrdinal(d3.schemeCategory10)
 
 function drawCircles(){
-	console.log("Hi I'm here")
+	// console.log("Hi I'm here")
 	let r = Math.random() * 10.0;
-	console.log("colorScale is ", colorScale);
-	console.log("colorScale (1) is ", colorScale(1));
-	console.log("colorScale (2) is ", colorScale(2));
-	console.log("colorScale (3) is ", colorScale(3));
-	console.log("colorScale (4) is ", colorScale(4));
-	console.log("colorScale (5) is ", colorScale(5));
-	colorScale(Math.random() * 10)
+	// console.log("colorScale is ", colorScale);
+	// console.log("colorScale (1) is ", colorScale(1));
+	// console.log("colorScale (2) is ", colorScale(2));
+	// console.log("colorScale (3) is ", colorScale(3));
+	// console.log("colorScale (4) is ", colorScale(4));
+	// console.log("colorScale (5) is ", colorScale(5));
+	// colorScale(Math.random() * 10)
 	d3.select('svg')
 		.append('circle')
 		.attr('cx', Math.random() * 500)
 		.attr('cy', Math.random() * 500)
 		.attr('r', "" + Math.max(r, 3))
-		.attr('fill', 'black');
+		.attr('fill', colorScale(Math.random() * 10));
 }
 d3.select('#shapeAdder').on('click', drawCircles)
